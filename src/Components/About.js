49bbfc4aa17fdd04f016c33e1fc5247e9f8f8d4c
Charalampos Leontidis <chleontidis@yahoo.com>
2023-03-23 13:49:10 +0200

    Add members page

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,12 +2,25 @@ import React from "react";
 import '../Styles/App.scss'
 import '../Styles/About.scss'
 import padouva from '../Images/Members/members_padouva.jpg';
-import efasouli from '../Images/Members/members_efasouli.jpg';
 import papadakis from '../Images/Members/members_papadakis.jpg';
 import cleontidis from '../Images/Members/members_cleontidis.jpg';
+import defaultIcon from './home_logo.png';
 import {FormattedMessage} from 'react-intl';
 
 export default function About(){
+
+    function showCv(e){
+        let id = e.target.parentElement.parentElement.parentElement.id
+        document.getElementById(id).lastElementChild.style.display = 'flex'
+        document.getElementById(id).firstElementChild.lastElementChild.lastElementChild.style.display = 'none'
+    }
+
+    function hideCv(e){
+        let id = e.target.parentElement.parentElement.id
+        document.getElementById(id).lastElementChild.style.display = 'none'
+        document.getElementById(id).firstElementChild.lastElementChild.lastElementChild.style.display = 'block'
+    }
+
     return(
         <div id="about-page-container">
             <div id="about-texts">
@@ -26,88 +39,164 @@ export default function About(){
             <div id="members">
                 <span className="title"><FormattedMessage id='members'/></span>
                 <div className="member-line">
-                    <div className="member">
-                        <img className="member-photo" src={padouva}/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members1-mame'/></span>
-                            <span><FormattedMessage id='president'/></span>
-                            <span><button>More</button></span>
-                        </div>
-                    </div>
-                    <div className="member">
-                        <img className="member-photo"/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members2-mame'/></span>
-                            <span><FormattedMessage id='secretary'/></span>
-                        </div>
-                    </div>
-                    <div className="member">
-                        <img className="member-photo"/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members3-mame'/></span>
-                            <span><FormattedMessage id='treasurer'/></span>
-                        </div>
-                    </div>
-                </div>
-                <div className="member-line">
-                    <div className="member">
-                        <img className="member-photo"/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members4-mame'/></span>
-                            <span><FormattedMessage id='member'/></span>
-                        </div>
-                    </div>
-                    <div className="member">
-                        <img className="member-photo"/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members5-mame'/></span>
-                            <span><FormattedMessage id='member'/></span>
-                        </div>
-                    </div>
-                    <div className="member">
-                        <img className="member-photo" src={efasouli}/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members6-mame'/></span>
-                            <span><FormattedMessage id='member'/></span>
-                        </div>
-                    </div>
-                </div>
-                <div className="member-line">
-                    <div className="member">
-                        <img className="member-photo" src={papadakis}/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members7-mame'/></span>
-                            <span><FormattedMessage id='member'/></span>
+                    <div className="member" id='padouva'>
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={padouva}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members1-mame'/></span>
+                                <span><FormattedMessage id='president'/> - <FormattedMessage id='members1-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
                             </div>
-                    </div>
-                    <div className="member">
-                        <img className="member-photo"/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members8-mame'/></span>
-                            <span><FormattedMessage id='member'/></span>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members1-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
                         </div>
                     </div>
-                    <div className="member">
-                        <img className="member-photo"/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members9-mame'/></span>
-                            <span><FormattedMessage id='member'/></span>
+                    <div className="member" id='stamataki'>
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={defaultIcon}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members2-mame'/></span>
+                                <span><FormattedMessage id='secretary'/> - <FormattedMessage id='members2-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members2-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
+                        </div>
+                    </div>
+                    <div className="member" id='vfasouli'>
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={defaultIcon}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members3-mame'/></span>
+                                <span><FormattedMessage id='treasurer'/> - <FormattedMessage id='members3-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members3-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
                         </div>
                     </div>
                 </div>
                 <div className="member-line">
-                    <div className="member">
-                        <img className="member-photo"/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members10-mame'/></span>
-                            <span><FormattedMessage id='member'/></span>
+                    <div className="member" id="skepetzaki">
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={defaultIcon}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members4-mame'/></span>
+                                <span><FormattedMessage id='member'/> - <FormattedMessage id='members4-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members4-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
+                        </div>
+                    </div>
+                    <div className="member" id="aleontidis">
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={defaultIcon}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members5-mame'/></span>
+                                <span><FormattedMessage id='member'/> - <FormattedMessage id='members5-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members5-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
+                        </div>
+                    </div>
+                    <div className="member" id="efasouli">
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={defaultIcon}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members6-mame'/></span>
+                                <span><FormattedMessage id='member'/> - <FormattedMessage id='members6-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members6-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="member-line">
+                    <div className="member" id="papadakis">
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={papadakis}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members7-mame'/></span>
+                                <span><FormattedMessage id='member'/> - <FormattedMessage id='members7-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members7-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
+                        </div>
+                    </div>
+                    <div className="member" id="tzanidakis">
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={defaultIcon}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members8-mame'/></span>
+                                <span><FormattedMessage id='member'/> - <FormattedMessage id='members8-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members8-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
+                        </div>
+                    </div>
+                    <div className="member" id="kavgalaki">
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={defaultIcon}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members9-mame'/></span>
+                                <span><FormattedMessage id='member'/> - <FormattedMessage id='members9-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members9-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="member-line">
+                    <div className="member" id="touloupa">
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={defaultIcon}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members10-mame'/></span>
+                                <span><FormattedMessage id='member'/> - <FormattedMessage id='members10-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members10-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
                         </div>
                     </div>                
-                    <div className="member">
-                        <img className="member-photo" src={cleontidis}/>
-                        <div className="member-text">
-                            <span><FormattedMessage id='members11-mame'/></span>
-                            <span><FormattedMessage id='member'/></span>
+                    <div className="member" id="cleontidis">
+                        <div className="member-text-photo-container">
+                            <img className="member-photo" src={cleontidis}/>
+                            <div className="member-text">
+                                <span><FormattedMessage id='members11-mame'/></span>
+                                <span><FormattedMessage id='member'/> - <FormattedMessage id='members11-title'/></span>
+                                <span id="do-arr" onClick={event=>showCv(event)} className="about-page-arrow"/>
+                            </div>
+                        </div>
+                        <div className="member-cv-container">
+                            <span><FormattedMessage id='members11-text'/></span>
+                            <span id="up-arr" onClick={event=>hideCv(event)} className="about-page-arrow"/>
                         </div>
                     </div>
                 </div>
