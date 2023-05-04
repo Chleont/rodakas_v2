091ff12014e1dc9ebb0f9b5e4e151423e4d67657
Charalampos Leontidis <chleontidis@yahoo.com>
2023-05-04 14:55:47 +0300

    Export click points for each house to file

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -10,7 +10,10 @@ export default function Map(){
     const map = {name:'aa',areas:json}
     const [width, setWidth] = useState(null)
     const [localCoords, setLocalCoords] = useState({x: 0, y: 0});
-    
+    const [houses, setHouses] = useState([])
+    const [coords, setCoords] = useState([])
+    const [newHouse, setNewHouse] = useState(true)
+
 
     useEffect(()=>{
         let container = document.getElementById('map-page-container')
@@ -26,21 +29,73 @@ export default function Map(){
         });
       };
     
-      useEffect(() => {
-        document.getElementById('mapper-container').addEventListener('mousemove', handleMouseMove);
-        console.log(document.getElementById('mapper-container'))
-        return () => {
-            document.getElementById('mapper-container').removeEventListener(
-            'mousemove',
-            handleMouseMove,
-          );
-        };
-      }, []);
+    useEffect(() => {
+      document.getElementById('mapper-container').addEventListener('mousemove', handleMouseMove);
+      console.log(document.getElementById('mapper-container'))
+      return () => {
+          document.getElementById('mapper-container').removeEventListener(
+          'mousemove',
+          handleMouseMove,
+        );
+      };
+    }, []);
 
+
+    function passCoords(){
+      if(!newHouse){
+        let coordis = coords
+        coordis.push(parseInt(document.getElementById('x').innerHTML.substring(1),10))
+        coordis.push(parseInt(document.getElementById('y').innerHTML.substring(0,document.getElementById('y').innerHTML.length - 1),10))
+        setCoords(coordis)
+      }else{
+        console.log('inNewHouse')
+        let coordis = []      
+        coordis.push(parseInt(document.getElementById('x').innerHTML.substring(1),10))
+        coordis.push(parseInt(document.getElementById('y').innerHTML.substring(0,document.getElementById('y').innerHTML.length - 1),10))
+        setCoords(coordis)
+        setNewHouse(false)
+      }
+    }
+
+    function startRec(){
+      setHouses([])
+      setCoords([])
+      document.getElementById('mapper-container').addEventListener('click',passCoords);
+    }
+
+    function handleEnter(e){
+      if(e.key === 'Enter'){
+        e.preventDefault();
+        let houseis = houses
+        let ccoo = coords
+        houseis.push({
+          id:e.target.value,
+          name:e.target.value,
+          coords:[...ccoo]
+        })
+        setHouses(houseis)
+        setNewHouse(true)
+        console.log(houses,houseis)
+      }
+    }
+
+    function completeRec(){
+      const element = document.createElement("a");
+      const textFile = new Blob([JSON.stringify(houses)], {type: 'text/plain'}); 
+      element.href = URL.createObjectURL(textFile);
+      element.download = "userFile.txt";
+      document.body.appendChild(element); 
+      element.click();
+    }
 
     return(
         <div id="map-page-container">
-            <span>({localCoords.x}, {localCoords.y})</span>
+            <span id='x'>({localCoords.x}</span><span>,</span><span id='y'>{localCoords.y})</span>
+            <button onClick={()=>{startRec()}}>Start</button>
+            <button onClick={()=>{completeRec()}}>Completed</button>
+            <input id="houseInput"
+              onKeyDown={(e)=>{handleEnter(e)}}
+            />
             <div id='mapper-container'>
                 <ImageMapper id='mapper'
                     src={mapNoBack} 
