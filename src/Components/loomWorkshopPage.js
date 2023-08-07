919a6b114708d6fef2f9a809342aca1a73195652
Charalampos Leontidis <chleontidis@yahoo.com>
2023-08-07 18:52:21 +0300

    Create a more agile way of posting workshops and post weaving workshop

--- src/Components/loomWorkshopPage.js
+++ src/Components/loomWorkshopPage.js
@@ -3,7 +3,7 @@ import '../Styles/Stoneworkshop.scss'
 import langfileGreek from '../Lang/el.json'
 import langfileEnglish from '../Lang/en.json'
 import { useIntl} from 'react-intl'
-import Somaki from '../Images/Instructors/Somaki.jpg'
+// import Somaki from '../Images/Instructors/Somaki.jpg'
 
 
 export default function StoneworkshopPage(){
@@ -12,7 +12,14 @@ export default function StoneworkshopPage(){
     var locale = lang.locale
     const[workshop, setWorkshop] = useState(langfileGreek.loomworkshop)
 
+    function importAll(r) {
+        return r.keys().map(r)
+    }
+      
+    const instImages = importAll(require.context('../Images/Instructors', false, /\.(png|jpe?g|svg)$/))
+
     useEffect(()=>{
+
         if(locale === 'el')
         {
             setWorkshop(langfileGreek.loomworkshop)
@@ -26,52 +33,69 @@ export default function StoneworkshopPage(){
             <div id='sw-header'>
                 <span>{workshop.title}</span>
                 <span>{workshop.dates}</span>
+                <span><i>{workshop.text_slogan}</i></span>
             </div>
             <div id='sw-info'>
-                <span>{workshop.introtext}</span>
-                <span>{workshop.info1}</span>
-                <span>{workshop.info2}</span>
-                <span className="sw-subtitle">{workshop.Programtitle}</span>
-                <span className="sw-activity-date">{workshop.program0}</span>
-                <span className="sw-activity-date">{workshop.program1.date}</span>
-                <span className="sw-activity">11:00 - 13:00: {workshop.program1.morning}</span>
-                <span className="sw-activity">18:30 - 20:30: {workshop.program1.evening}</span>
-                <span className="sw-activity-date">{workshop.program2.date}</span>
-                <span className="sw-activity">20:30 - 21:30: {workshop.program2.evening}</span>
-                <span className="sw-activity-date">{workshop.program3.date}</span>
-                <span className="sw-activity">20:30 - 21:30: {workshop.program3.evening}</span>
-                <span className="sw-activity-date">{workshop.program4.date}</span>
-                <span className="sw-activity">17:00 - 19:00: {workshop.program4.evening}</span>
-                <span className="sw-activity-date">{workshop.program5.date}</span>
-                <span className="sw-activity">20:00 - 21:00: {workshop.program5.evening}</span>
-                <span className="sw-activity-date">{workshop.program5.date}</span>
-                <span className="sw-activity">09:30 - 14:00: {workshop.program6.morning}</span>
-                <span className="sw-activity">18:30 - 20:30: {workshop.program6.evening}</span>
+                {workshop.introtext.map(each => <p className='introtext' key={workshop.introtext.indexOf(each)}>{each}</p>)}
+                <span id="info-title" className="sw-subtitle">{workshop.Programtitle}</span>
+                {/* Program */}
+                {workshop.program.map(each => 
+                    <span className='program-element' key={workshop.program.indexOf(each)}>
+                        <p>{each.date}</p>
+                        {each.activities.map(text => <p className='program-text paragraph' key={each.activities.indexOf(text)}>{text}</p>)}
+                    </span>
+                )}
+                {/* Accommodation */}
                 <span className="sw-subtitle">{workshop.accommodation}</span>
-                <span>{workshop.accommodationInfo1}</span>
-                <span className="sw-subtitle">{workshop.accommodationHouses}</span>
-                <span>{workshop.accommodationHouses1}</span>
-                <span>{workshop.accommodationHouses2}</span>
-                <span>{workshop.accommodationHouses3}</span>
-                <span>{workshop.accommodationHouses4}</span>
+                {workshop.accommodationInfo.map(each => 
+                    <p className='accomodation-text paragraph' id={workshop.accommodationInfo.indexOf(each)==0?'accomodation-free':''} key={workshop.accommodationInfo.indexOf(each)}>{each}</p>
+                )}
             </div>
             <div id='sw-inscription'>
                 <span className="sw-subtitle">{workshop.costAndRegistration}</span>
-                <span>{workshop.cost}</span>
+                <span className="paragraph">{workshop.cost}</span>
                 <a href={workshop.formlink} target='_blank'>{workshop.registration}</a>
             </div>
+
+            {/* Instructors */}
             <div id='sw-instructors'>
                 <span className="sw-subtitle">{workshop.instructorTitle}</span>
+                {workshop.instructors.map(each => { 
+                    if(each.image){
+                        var imgFile = ''
+
+                        instImages.map(img =>{
+                            if(img.default.includes(each.image)){
+                                imgFile = img.default
+                            }
+                        })
+                    }
+                    return(
+                        <div key={workshop.instructors.indexOf(each)} className='instructor'>
+                            <p className="instructor-name">{each.name}</p>
+                            <span key={workshop.instructors.indexOf(each)}className="instructor-text">
+                                <img src={imgFile}/>
+                                {each.bio.map(bio =>{
+                                    if(bio.includes('\n')){
+                                        return(
+                                            <p className='bio-special' key={each.bio.indexOf(bio)}>{bio}</p>
+                                        )
+                                    }else return(
+                                        <p key={each.bio.indexOf(bio)}>{bio}</p>
+                                    )
+                                })}
+                            </span>
+                        </div>
+                    )
+                })}
                 <span className="instructor-name">{workshop.instructorName1}</span>
                 <span className="instructor-text">
-                    <img src={Somaki}/>
                     <span>{workshop.instructorBio1}</span>
                     <br/>
                     <span>{workshop.instructorBio11}</span>
                 </span>
                 <span className="instructor-name">{workshop.instructorName2}</span>
                 <span className="instructor-text">
-                    {/* <img src={Vergianos}/> */}
                     <span>{workshop.instructorBio2}</span>
                 </span>
             </div>
