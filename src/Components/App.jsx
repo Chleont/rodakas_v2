0ae0a0b2eb7c5780e5271c550e5278777232124f
Charalampos Leontidis <chleontidis@yahoo.com>
2024-11-06 14:15:54 +0200

    fix eslint errors

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,81 +1,81 @@
-import React, {useEffect, useState} from 'react'
-import {IntlProvider} from 'react-intl'
-import {FormattedMessage} from 'react-intl'
-import {Route, useNavigate,Routes} from 'react-router-dom'
-import '../Styles/App.scss'
-import Greek from '../Lang/el.json'
-import English from '../Lang/en.json'
-import logo from './home_logo.png'
-import NavbarBrowserMenu from './NavbarBrowserMenu.jsx'
-import NavbarMobileMenu from './NavbarMobileMenu.jsx'
-import Contact from './Contact.jsx'
-import Archive from './Archive.jsx'
-import Footer from './Footer.jsx'
-import Workshops from './Workshops.jsx'
-import Multimedia from './Multimedia.jsx'
-import About from './About.jsx'
-import Map from './Map.jsx'
+import React, { useEffect, useState } from 'react';
+import { IntlProvider } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
+import { Route, useNavigate, Routes } from 'react-router-dom';
+import '../Styles/App.scss';
+import Greek from '../Lang/el.json';
+import English from '../Lang/en.json';
+import logo from './home_logo.png';
+import NavbarBrowserMenu from './NavbarBrowserMenu.jsx';
+import NavbarMobileMenu from './NavbarMobileMenu.jsx';
+import Contact from './Contact.jsx';
+import Archive from './Archive.jsx';
+import Footer from './Footer.jsx';
+import Workshops from './Workshops.jsx';
+import Multimedia from './Multimedia.jsx';
+import About from './About.jsx';
+import Map from './Map.jsx';
 
 
 function App() {
 
 
-    const [locale, setLocale] = useState('el')
-    const [messages, setMessages] = useState(Greek)
-    const [isMobile, setWidth] = useState(window.innerWidth < 1000)
+    const [locale, setLocale] = useState('el');
+    const [messages, setMessages] = useState(Greek);
+    const [isMobile, setWidth] = useState(window.innerWidth < 1000);
     const frogOptions = {
-        size:4, 
+        size: 4,
         units: 'em',
         fontSize: 20,
-        startingPosition:{
-            x:0,
-            y:0
-        }, 
-        timeToOpenMouth:2000,
-        timeToAttack:1000,
+        startingPosition: {
+            x: 0,
+            y: 0
+        },
+        timeToOpenMouth: 2000,
+        timeToAttack: 1000,
         navbarHeight: 84
-    }
+    };
 
-    let navigate = useNavigate()
+    let navigate = useNavigate();
 
     function handleWindowSizeChange() {
-        setWidth(window.innerWidth < 1000)
+        setWidth(window.innerWidth < 1000);
     }
 
     useEffect(() => {
-        window.addEventListener('resize', handleWindowSizeChange)
+        window.addEventListener('resize', handleWindowSizeChange);
         return () => {
-            window.removeEventListener('resize', handleWindowSizeChange)
-        }
-    }, [])
+            window.removeEventListener('resize', handleWindowSizeChange);
+        };
+    }, []);
 
 
-    function routeChange(path){
-        return(
+    function routeChange(path) {
+        return (
             navigate(path)
-        )
+        );
     }
 
     return (
         <div className="App">
-            <IntlProvider locale={locale} defaultLocale = 'el' messages={messages}>
+            <IntlProvider locale={locale} defaultLocale='el' messages={messages}>
                 <div id='navbar' className='Navbar'>
-                    {isMobile ? <NavbarMobileMenu/>:<NavbarBrowserMenu/>}
+                    {isMobile ? <NavbarMobileMenu /> : <NavbarBrowserMenu />}
                     <button className="noOutlineButton navbarButton" id="pick-language-button" onClick={
-                        function(){
-                            if(messages === Greek){
-                                setMessages(English)
-                                setLocale('en')
-                            }else{
-                                setMessages(Greek)
-                                setLocale('el')
+                        function () {
+                            if (messages === Greek) {
+                                setMessages(English);
+                                setLocale('en');
+                            } else {
+                                setMessages(Greek);
+                                setLocale('el');
                             }
                         }
                     }>
                         <span id='pick-language-icon'></span>
                         <span id="pick-language-text">
-                            <FormattedMessage id = "language"/>     
-                        </span>         
+                            <FormattedMessage id="language" />
+                        </span>
                     </button>
                 </div>
                 <Routes>
@@ -83,25 +83,25 @@ function App() {
                         <div className="homescreen">
                             <img src={logo} className="app-logo" alt="logo" />
                             <p className='app-title'>
-                                <FormattedMessage id = "homescreen-title"/>     
+                                <FormattedMessage id="homescreen-title" />
                             </p>
                             <div className='footer'>
-                                <Footer/>
+                                <Footer />
                             </div>
                         </div>
-                    }/>
+                    } />
                     <Route path='/about' element={
-                        <About/>
-                    }/>
+                        <About />
+                    } />
                     <Route path='/activity' element={
-                        <Workshops url={''}/>
-                    }/>
+                        <Workshops url={''} />
+                    } />
                     <Route path='/activity/stoneworkshop' element={
-                        <Workshops url={'/stoneworkshop'}/>
-                    }/>
+                        <Workshops url={'/stoneworkshop'} />
+                    } />
                     <Route path='/activity/loomworkshop' element={
-                        <Workshops url={'/loomworkshop'}/>
-                    }/>
+                        <Workshops url={'/loomworkshop'} />
+                    } />
                     {/* <Route path='/activity/stonecarving' element={
                         <Workshops url={'/stonecarving'}/>
                     }/> */}
@@ -109,21 +109,21 @@ function App() {
                         <Workshops url={'/feast23'}/>
                     }/> */}
                     <Route path='/archive' element={
-                        <Archive/>
-                    }/>
+                        <Archive />
+                    } />
                     <Route path='/multimedia' element={
-                        <Multimedia/>
-                    }/>
+                        <Multimedia />
+                    } />
                     <Route path='/multimedia/family-islands' element={
-                        <Map/>
-                    }/>
+                        <Map />
+                    } />
                     <Route path='/contact' element={
-                        <Contact/>
-                    }/>
+                        <Contact />
+                    } />
                 </Routes>
             </IntlProvider>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
